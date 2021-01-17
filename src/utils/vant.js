6f94117bbed509e968faf1887d9ec8616d6a5ce6
Matin <2216398985@qq.com>
2021-01-17 14:18:37 +0800

    登陆功能完成

--- src/utils/vant.js
+++ src/utils/vant.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 import {
 	Button,
 	Form,
-	Field 
+	Field,
+	Toast 
 } from 'vant'
 
 // 全局注册
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(Field)
+Vue.use(Toast)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,13 @@
 	<hm-header>登陆</hm-header>
 	<hm-logo></hm-logo>
 	<!-- 登陆表单 -->
-	<van-form @submit="onSubmit">
+	<van-form @submit="onLogin">
 	<van-field
 		v-model="username"
 		name="用户名"
 		label="用户名"
 		placeholder="请输入账号"
-		:rules="[{ required: true, message: '请填写用户名' },
-		{pattern:/^\d{5,9}$/, message: ''}
-		]"
+		:rules="rules.username"
 	/>
 	<van-field
 		v-model="password"
@@ -19,7 +17,7 @@
 		name="密码"
 		label="密码"
 		placeholder="请输入密码"
-		:rules="[{ required: true, message: '请填写密码' }]"
+		:rules="rules.password"
 	/>
 	<div style="margin: 16px;">
 		<van-button round block type="info" native-type="submit">提交</van-button>
@@ -29,16 +27,39 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
   	return {
-  	username: '',
-  	password: ''
+  	  username: '',
+	  password: '',
+	  rules: {
+		username: [
+		  { required: true, message: '请填写用户名', trigger: 'onChange' },
+		  { pattern: /^\d{5,9}$/, message: '请输入5-9位数字账号', trigger: 'onChange' }
+		],
+		password: [
+		  { required: true, message: '请填写密码', trigger: 'onChange' },
+		  { pattern: /^\d{3,9}$/, message: '请输入5-9位数字密码', trigger: 'onChange' }
+		]
+	  }
   	}
   },
   methods: {
-	onLogin() {
-	  console.log('登陆')
+	async onLogin() {
+	  const res = await axios.post('http://localhost:3000/login', {
+		username: this.username,
+		password: this.password
+	  })
+	  console.log(res)
+	  const { statusCode, message } = res.data
+	  if (statusCode === 200) {
+		this.$toast.success(message)
+		// 保存token，跳转个人中心
+		this.$router.push('/user')
+	  } else {
+		this.$toast.fail(message)
+	  }
 	}
   }
 }
